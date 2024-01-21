d5340744d6bcef272754dc11ac64a614d7a55ac1
mykangelo <mmykangelo@gmail.com>
2024-01-21 16:26:16 +0800

    Answer for exercise number 4

---
+++ exercise4.py
@@ -0,0 +1,17 @@
+
+
+#store a string or word in a variable 
+a_word = "Anatomy"
+
+print("Removing letters from the word")
+
+#print the original word and the insert the string slicing function with the another print of the word 
+print("Original word: ", a_word)
+print(a_word[4:])
+
+#print the original word and the insert the string slicing function with the another print of the word 
+print("Original word: ", a_word)
+print(a_word[2:])
+
+
+
